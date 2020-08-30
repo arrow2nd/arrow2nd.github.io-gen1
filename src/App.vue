3d109564f31db86552ac7d8021dc184328df3d69
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2020-08-30 19:07:20 +0900

    fix: 背景色を反映

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,11 @@ export default {
 </script>
 
 <style>
-body {
-  background-color: #faf8f7;
-}
-
 #app {
   font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2e2e2e;
   padding-bottom: 50px;
+  color: #2e2e2e;
 }
 </style>
